e173d73c4c5307051c4c554a8d7fd30edf981257
TiffanyJan <tiffany_low@hotmail.com>
2020-07-27 22:25:14 +0100

    refresh input box each time

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import RecipieList from "./RecipieList";
 import "./App.css";
 
 function App() {
-  const [todos, setTodos] = useState([
-    { id: 1, name: "madusha", complete: false },
-    { id: 2, name: "tiff", complete: true },
-  ]);
+  const [todos, setTodos] = useState([]);
+  const todoNameRef = useRef();
+
+  function handleAddTodo() {
+    const name = todoNameRef.current.value;
+
+    if (name == "") return;
+    todoNameRef.current.value = null;
+  }
 
   return (
     <>
       <RecipieList todos={todos} />
-      <input type="text"></input>
-      <button>Add Tasks</button>
+      <input ref={todoNameRef} type="text"></input>
+      <button onClick={handleAddTodo}>Add Tasks</button>
       <button>Clear Tasks</button>
       <div>Tasks Left:</div>
     </>
