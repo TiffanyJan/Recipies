54f350423b4c274d40b6a91a704a71575d56812f
TiffanyJan <tiffany_low@hotmail.com>
2020-07-28 22:59:20 +0100

    use effect added

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
-import React, { useState, useRef } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import RecipieList from "./RecipieList";
 import "./App.css";
+
+const LOCAL_STORAGE_KEY = "todoApp.todos";
 
 function App() {
   const [todos, setTodos] = useState([]);
   const todoNameRef = useRef();
 
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos));
+  }, [todos]);
+
   function handleAddTodo(e) {
     const name = todoNameRef.current.value;
     setTodos((prevTodos) => {
-      return [...prevTodos,
+      return [
+        ...prevTodos,
         { id: todos.length + 1, name: name, complete: false },
       ];
     });
