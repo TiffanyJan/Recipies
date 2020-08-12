c6c8002aad109a55bab55d814b5631497e66eb93
TiffanyJan <tiffany_low@hotmail.com>
2020-08-12 22:22:00 +0100

    insert navbar for new recipies page

--- src/App.js
+++ src/App.js
@@ -1,28 +1,31 @@
-import React, { useState} from "react";
+import React from "react";
 import { Route, Switch, Link } from "react-router-dom";
+import { Navbar, Nav } from "react-bootstrap";
 import NewRecipiesPage from "./NewRecipiesPage";
 import HomePage from "./HomePage";
 
-
 function App() {
-
-  
-
   return (
     <>
-
-     <main>
-    <Switch> 
-    <Route path='/NewRecipiesPage' component={NewRecipiesPage} />
-    <Route path ='/' component ={HomePage}/>
-    </Switch>
-    </main>
-
-    
+      <Navbar className="color-nav" variant="light">
+        <Navbar.Brand href="#home">Recipies</Navbar.Brand>
+        <Nav className="mr-auto">
+          <Nav.Link href="#home">Home</Nav.Link>
+          <Nav.Link href="#new-recipies">
+            {" "}
+            <Link to="/NewRecipiesPage">New Recipies</Link>{" "}
+          </Nav.Link>
+          <Nav.Link href="#popular-recipies">Popular Recipies</Nav.Link>
+        </Nav>
+      </Navbar>
+      <main>
+        <Switch>
+          <Route path="/NewRecipiesPage" component={NewRecipiesPage} />
+          <Route path="/" component={HomePage} />
+        </Switch>
+      </main>
     </>
-    
-    
   );
-  }
+}
 
 export default App;
