4e49d28681ec4e5e10b5df59778bc529aef51675
TiffanyJan <tiffany_low@hotmail.com>
2020-08-16 12:14:57 +0100

    get rid of html in text summary

--- src/Recipie.js
+++ src/Recipie.js
@@ -2,6 +2,14 @@ import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 export default function Recipie(props) {
+    function stripHtml(summary)
+    {
+       var tmp = document.createElement("DIV");
+       tmp.innerHTML = summary;
+       return tmp.textContent || tmp.innerText || "";
+    }
+
+
   const showRecipe = (recipeDetails) => {
     if (recipeDetails) {
       console.log(recipeDetails);
@@ -11,7 +19,12 @@ export default function Recipie(props) {
             <div className="heading">
               <h1>{recipeDetails.title}</h1>
             </div>
-            {recipeDetails.summary}
+            <div className="row mt-2">
+            {stripHtml(recipeDetails.summary )}
+            </div>
+            <div className="row mt-2">
+            Servings:{recipeDetails.servings}
+            </div>
             <div className="row mt-2">
               <img src={recipeDetails.image}></img>
             </div>
