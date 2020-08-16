beac1e2ab3a08c88e2d2ef6eca95bffbdb3694e0
TiffanyJan <tiffany_low@hotmail.com>
2020-08-16 13:02:11 +0100

    insert new function to detect instructions

--- src/Recipie.js
+++ src/Recipie.js
@@ -2,13 +2,21 @@ import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 export default function Recipie(props) {
-    function stripHtml(summary)
-    {
-       var tmp = document.createElement("DIV");
-       tmp.innerHTML = summary;
-       return tmp.textContent || tmp.innerText || "";
-    }
 
+  function stripHtml(summary) {
+    var tmp = document.createElement("DIV");
+    tmp.innerHTML = summary;
+    return tmp.textContent || tmp.innerText || "";
+  }
+
+  function instructions(recipeDetails){
+      if(recipeDetails.instructions){
+          return recipeDetails.instructions
+      }
+      else{
+          return recipeDetails.sourceUrl
+      }
+  }
 
   const showRecipe = (recipeDetails) => {
     if (recipeDetails) {
@@ -20,13 +28,16 @@ export default function Recipie(props) {
               <h1>{recipeDetails.title}</h1>
             </div>
             <div className="row mt-2">
-            {stripHtml(recipeDetails.summary )}
+              <div className="col-sm">{stripHtml(recipeDetails.summary)}</div>
             </div>
-            <div className="row mt-2">
-            Servings:{recipeDetails.servings}
-            </div>
+            <div className="row mt-2">Servings:{recipeDetails.servings}</div>
             <div className="row mt-2">
               <img src={recipeDetails.image}></img>
+            </div>
+            <div className="row mt-2">
+              <div className="col-sm">
+                Instructions: {instructions(recipeDetails)}
+              </div>
             </div>
           </div>
         </>
